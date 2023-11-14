7b9baf71444f7796ff72245c11737d8689a13207
Annabelle Blum <annabelleb4509@gmail.com>
2023-11-14 11:29:47 +1100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import ToDoList from "./ToDoList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React To-Do</h1>
+      <ToDoList />
+    </div>
+  );
+}
+
+
+
+export default App;
